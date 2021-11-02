4286fc9a6411b43cb07092523082bc6e5a386f5e
yaredow <yaredyilma11@gmail.con>
2021-11-02 13:31:22 +0300

    challenge three

---
+++ day2/challenge_three/Your_life_in_weeks.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+
+
+age_as_int = int(age)
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52 
+months_remaining = years_remaining * 12
+
+message = f"you have {days_remaining} days, {weeks_remaining} weeks, {months_remaining} months, and {years_remaining} years remaining"
+print(message)
+
+
