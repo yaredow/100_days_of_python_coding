b266530dd3d8f2da91f374d8746c014480cc8f38
yaredow <yaredyilma11@gmail.con>
2021-11-02 11:04:57 +0300

    challenge 1

---
+++ day2/challenge_one/challenge_one.py
@@ -0,0 +1,14 @@
+# Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a = two_digit_number[0]
+b = two_digit_number[1]
+result = int(a) + int(b)
+print(result)
+
+
+
