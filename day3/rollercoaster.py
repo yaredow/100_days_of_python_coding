bdece69b571d2db10f79af31895408b6dfa073bd
yaredow <yaredyilma11@gmail.con>
2021-11-04 08:07:52 +0300

    day three

--- day3/rollercoaster.py
+++ day3/rollercoaster.py
@@ -4,5 +4,5 @@ height = int(input("what is your height in cm? "))
 if height > 120:
 	print("You can ride the rollercoaster.")
 else:
-	print("Sorry you should come back when you are a little older")
+	print("Sorry you should come back when you are a little taller")
 
