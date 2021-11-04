f23d08a337f940d24856e691cec2c5b5edcc820d
yaredow <yaredyilma11@gmail.con>
2021-11-04 11:03:43 +0300

    remove some files

--- day3/rollercoaster.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the roalercoaster")
-height = int(input("what is your height in cm? "))
-
-if height >= 120:
-	print("You can ride the rollercoaster.")
-else:
-	print("Sorry you should come back when you are a little taller")
-
