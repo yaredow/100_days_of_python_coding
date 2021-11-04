4d044ea583bbf9e84e791676ccc3348c2e1bab14
yaredow <yaredyilma11@gmail.con>
2021-11-04 11:20:32 +0300

    age added

--- day3/rollercoaster/rollercoaster.py
+++ day3/rollercoaster/rollercoaster.py
@@ -3,6 +3,13 @@ height = int(input("what is your height in cm? "))
 
 if height >= 120:
 	print("You can ride the rollercoaster.")
+	age = int(input("What is your age? "))
+	if age <= 12:
+		print("please pay $5")
+	elif age <= 18:
+		print("please pay $7")
+	else: 
+		print("please pay $12")
 else:
 	print("Sorry you should come back when you are a little taller")
 
