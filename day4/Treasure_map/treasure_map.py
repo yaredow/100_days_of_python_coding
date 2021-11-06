e8fa11bc98955c3e299638ebb6041375e7f79468
yaredow <yaredyilma@gmail.com>
2021-11-06 18:26:52 +0300

    challenge three

---
+++ day4/Treasure_map/treasure_map.py
@@ -0,0 +1,11 @@
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+x_direction = int(position[0])
+y_direction = int(position[1])
+
+print(map[x_direction-1])
