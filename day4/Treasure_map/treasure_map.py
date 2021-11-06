d0db0239ba6eee205d87be3ec3f4ad0d8272fb3e
yaredow <yaredyilma@gmail.com>
2021-11-06 18:44:12 +0300

    changed the comment

--- day4/Treasure_map/treasure_map.py
+++ day4/Treasure_map/treasure_map.py
@@ -8,4 +8,7 @@ position = input("Where do you want to put the treasure? ")
 x_direction = int(position[0])
 y_direction = int(position[1])
 
-print(map[x_direction-1])
+selected_row = map[y_direction -1]
+selected_row[x_direction - 1] = 'X'
+# print the result
+print(f"{row1}\n{row2}\n{row3}")
