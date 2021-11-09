1a1ae12eff8a34cf47112c3e29ddf1a2b0e8f86a
yaredow <yaredyilma11@gmail.com>
2021-11-10 00:29:37 +0300

    added step four instruction

--- day7/Hangman/step_two.py
+++ day7/Hangman/step_two.py
@@ -11,14 +11,12 @@ print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 
+
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
-diplay = []
-size = len(chosen_word)
-for _ in range(size):
+for letter in chosen_word:
   display += "_"
-print(display)
 
 guess = input("Guess a letter: ").lower()
 
@@ -26,8 +24,8 @@ guess = input("Guess a letter: ").lower()
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-for position in range(size):
-  letter = chosen_word[position]
+for position in range(len(chosen_word)):
+  letter = chosen_word[position] 
   if letter == guess:
     display[position] = letter
 
