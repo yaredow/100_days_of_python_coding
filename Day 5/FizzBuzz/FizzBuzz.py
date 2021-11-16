4536597bd644583ee8efd962266c9b8e88a07c0b
yaredow <yaredyilma11@gmail.com>
2021-11-16 22:27:52 +0300

    changed the code completly

--- Day 5/FizzBuzz/FizzBuzz.py
+++ Day 5/FizzBuzz/FizzBuzz.py
@@ -1,11 +1,13 @@
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("Fizzbuzz")
-    elif number % 3 == 0:
-        print("Buzz")
-    elif number % 5 == 0:
-        print("Fizz")
-    else:
-        print(number)
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("fizzbuzz ", end="")
+        elif number % 3 == 0:
+            print("buzz ", end="")
+        elif number % 5 == 0:
+            print("Fizz ", end="")
+        else:
+            print("{} ".format(number), end="")
+fizzbuzz()        
 
   
