4e7f096259194417aa7df684872ce8ac27ad22ad
yaredow <yaredyilma11@gmail.com>
2021-11-22 23:48:26 +0300

    added global variable called turns

--- Day 12/Number_guessing_game.py/Number_guessing_game.py
+++ Day 12/Number_guessing_game.py/Number_guessing_game.py
@@ -3,31 +3,34 @@ import random
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURN = 5
 number = random.randint(1, 100)
-def guess_number(user_input, number):
+def guess_number(guess, answer, turns):
 
-        if user_input < number:
+        if guess < answer:
             print("Too law. \nGuess again")
-        elif user_input < number:
+            turns -= 1
+        elif guess < answer:
             print("Too high. \nGuess again")
+            turns -= 1
         else:
             game_over = True
             print("You got it. the answer was {}".format(number))
 
-def game_level():
+def set_difficulty():
+    chosen_difficulty = input("Choose a difficuty. Type 'easy' or 'hard': ").lower()
     if chosen_difficulty == "easy":
-        return "You have {} attempts to guess the game".format(EASY_LEVEL_TURNS)
+        return EASY_LEVEL_TURNS
     elif chosen_difficulty == "hard":
-        return "You have {} attempts to guess the game".format(HARD_LEVEL_TURN)
-    else:
-        return "Wrong input"
+        return HARD_LEVEL_TURN
+   
 
 def game():
     print("Welcome to the Number Guessing game")
     print("I'm Thinking of a number between 1 and 100")
-    print("Pssst, The correct answer is {}".format(random.randint(1, 100)))
-    chosen_difficulty = input("Choose a difficuty. Type 'easy' or 'hard': ").lower()
-    print(game_level())
-    while number != user_input:
-        user_input =int(input("Guess a number: "))
-        print(guess_number(user_input, number))
+    answer = random.randint(1, 100)
+    turns = set_difficulty()
+    guess = 0
+    while answer != guess:
+        print("You have {} attempts to guess the number!".format(turns))
+        guess =int(input("Guess a number: "))
+        turns = guess_number(guess, answer, turns)
 game()        
