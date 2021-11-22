7097781c2f80808fd3af3d14184ba598e00d8e8c
yaredow <yaredyilma11@gmail.com>
2021-11-23 00:27:25 +0300

    fixed infinate loop

--- Day 12/Number_guessing_game.py/Number_guessing_game.py
+++ Day 12/Number_guessing_game.py/Number_guessing_game.py
@@ -1,29 +1,28 @@
 import random
-
+from art import logo
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURN = 5
 number = random.randint(1, 100)
-def guess_number(guess, answer, turns):
-
-        if guess < answer:
-            print("Too law. \nGuess again")
-            turns -= 1
-        elif guess < answer:
-            print("Too high. \nGuess again")
-            turns -= 1
-        else:
-            game_over = True
-            print("You got it. the answer was {}".format(number))
-
 def set_difficulty():
     chosen_difficulty = input("Choose a difficuty. Type 'easy' or 'hard': ").lower()
     if chosen_difficulty == "easy":
         return EASY_LEVEL_TURNS
-    elif chosen_difficulty == "hard":
+    else:
         return HARD_LEVEL_TURN
    
+def guess_number(guess, answer, turns):
+
+        if guess < answer:
+            print("Too law. \nGuess again")
+            return turns - 1
+        elif guess > answer:
+            print("Too high. \nGuess again")
+            return turns - 1
+        else:
+            print("You got it. the answer was {}".format(number))
 
 def game():
+    print(logo)
     print("Welcome to the Number Guessing game")
     print("I'm Thinking of a number between 1 and 100")
     answer = random.randint(1, 100)
@@ -33,4 +32,6 @@ def game():
         print("You have {} attempts to guess the number!".format(turns))
         guess =int(input("Guess a number: "))
         turns = guess_number(guess, answer, turns)
-game()        
+        if turns == 0:
+            print("You are out of attempts")
+game()          
