73325871d13a7034ca70dfca673527e156b211ae
yaredow <yaredyilma11@gmail.com>
2021-11-23 12:18:45 +0300

    added if statement to break the while loop

--- Day 12/Number_guessing_game.py/Number_guessing_game.py
+++ Day 12/Number_guessing_game.py/Number_guessing_game.py
@@ -1,37 +1,40 @@
 import random
 from art import logo
-EASY_LEVEL_TURNS = 10
+EASY_LEVEL_TURN = 10
 HARD_LEVEL_TURN = 5
-number = random.randint(1, 100)
-def set_difficulty():
-    chosen_difficulty = input("Choose a difficuty. Type 'easy' or 'hard': ").lower()
-    if chosen_difficulty == "easy":
-        return EASY_LEVEL_TURNS
+def difficulty_level():
+    """Checks the randomly generated number with the guess"""
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    if level == "easy":
+        return EASY_LEVEL_TURN
     else:
         return HARD_LEVEL_TURN
-   
-def guess_number(guess, answer, turns):
-
-        if guess < answer:
-            print("Too law. \nGuess again")
-            return turns - 1
-        elif guess > answer:
-            print("Too high. \nGuess again")
-            return turns - 1
-        else:
-            print("You got it. the answer was {}".format(number))
+def guess_evaluater(guess, number, turns):
+    if guess > number:
+        print("Too low")
+        return turns -1
+    elif guess < number:
+        print("Too high")
+        return turns -1
+    else:
+        print("You got it. The answer was {}".format(number))
 
 def game():
     print(logo)
-    print("Welcome to the Number Guessing game")
-    print("I'm Thinking of a number between 1 and 100")
+    print("Welcome to a number guessing game!")
+    print("I'm thining of a number between 1 and 100")
     answer = random.randint(1, 100)
-    turns = set_difficulty()
+    turns = difficulty_level()
     guess = 0
-    while answer != guess:
-        print("You have {} attempts to guess the number!".format(turns))
-        guess =int(input("Guess a number: "))
-        turns = guess_number(guess, answer, turns)
+    while guess != answer:
+        print("You have {} attempts to guess the number.".format(turns))
+        guess = int(input("Make a guess: "))
+        turns = guess_evaluater(guess, answer, turns)
         if turns == 0:
-            print("You are out of attempts")
-game()          
+            print("You are out of attempts. You lose")
+        elif guess != answer:
+            print("Guess again")
+game()
+
+
+
