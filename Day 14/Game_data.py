1162658d7aa439700b46c62e4fb938fe2b20843f
yaredow <yaredyilma11@gmail.com>
2021-11-24 20:58:36 +0300

    created a separate game data

---
+++ Day 14/Game_data.py
@@ -0,0 +1,51 @@
+Clebritites_instagram = [
+    {
+        "Name": "Christiano Ronaldo",
+        "Profession": "Football player",
+        "country": "Portugal",
+        "Followers": 315.75
+    },
+    {
+        "Name": "The Rock",
+        "Profession": "Professional wrestler, Film actor, and Philanthropist",
+        "Country": "Haywar, California, USA",
+        "Followers": 254.76 
+    },
+    {
+        "Name": "Ariana Grande",
+        "Profession": "Singer",
+        "Country": "Boca Raton, Florida, USA",
+        "Followers": 252.64 
+    },
+    {
+        "Name": "The Weeknd",
+        "Profession": "Singer",
+        "Country": "Ethiopia",
+        "Followers": 34.74
+    },
+    {
+        "Name": "Teddy Afro",
+        "Profession": "Singer and song writer",
+        "Country": "Addis Ababa, Ethiopia",
+        "Followers": 1
+    },
+    {
+        "Name": "Selena Gomez",
+        "Profession": "Actress, producer, and song writer",
+        "Country": "Grand prairie, Texas, USA",
+        "Followers": 245.5
+    },
+    {
+        "Name": "Justin Bieber",
+        "Profession": "Singer and song writer",
+        "Country": "Canada",
+        "Followers": 183.79
+    },
+    {
+        "Name": "Lionel Messi",
+        "Profession": "Footbal player",
+        "Country": "Argentina",
+        "Followers": 233.79
+    }
+    
+    ]
