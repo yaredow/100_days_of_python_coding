a656a82f6cb1b7b3d4cd479da15517935782fc82
yaredow <yaredyilma11@gmail.com>
2021-11-25 08:57:15 +0300

    removed game function

--- Day 14/Higer_lower.py
+++ Day 14/Higer_lower.py
@@ -20,37 +20,37 @@ def check(number_of_followers_a, number_of_followers_b, user_guess):
     else:
         return False
 
-def game():
-    continue_operation = True
+
+continue_operation = True
+choice_b = random.choice(Celebritites_instagram)
+while continue_operation:
+    print(logo)
+    # generate a random dictionaries from the nested list
+    choice_a = choice_b
     choice_b = random.choice(Celebritites_instagram)
-    while continue_operation:
-        print(logo)
-        # generate a random dictionaries from the nested list
-        choice_a = choice_b
+    while choice_a == choice_b:
         choice_b = random.choice(Celebritites_instagram)
-        while choice_a == choice_b:
-            choice_b = random.choice(Celebritites_instagram)
-        # pick number of followers from the randomly generated dictionery
-        number_of_followers_a = choice_a["Followers"]
-        number_of_followers_b = choice_b["Followers"]
+    # pick number of followers from the randomly generated dictionery
+    number_of_followers_a = choice_a["Followers"]
+    number_of_followers_b = choice_b["Followers"]
 
-        # Check if both the generated values are equal. if they are equal generated the second one again 
-        if choice_a == choice_b:
-            choice_b = random.choice(Celebritites_instagram)
-        print("Compare A: {}".format(format_data(choice_a)))
-        print(vs)
-        print("Against B: {}".format(format_data(choice_b)))
+    # Check if both the generated values are equal. if they are equal generated the second one again 
+    if choice_a == choice_b:
+        choice_b = random.choice(Celebritites_instagram)
+    print("Compare A: {}".format(format_data(choice_a)))
+    print(vs)
+    print("Against B: {}".format(format_data(choice_b)))
 
-        # recieve user guess and store it in avariable called "guess"
-        user_guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-        
-        score = 0
-        follower_check = check(number_of_followers_a, number_of_followers_b, user_guess)
-        if follower_check == True:
-            score += 1
-            print("You are right. The current score: {}".format(score))
-        else:
-            print("Sorry, that's wrong. Final score: {}".format(score))
-            continue_operation = False
-game()
+    # recieve user guess and store it in avariable called "guess"
+    user_guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    score = 0
+    follower_check = check(number_of_followers_a, number_of_followers_b, user_guess)
+    if follower_check == True:
+        score += 1
+        print("You are right. The current score: {}".format(score))
+    else:
+        print("Sorry, that's wrong. Final score: {}".format(score))
+        continue_operation = False
+
 
