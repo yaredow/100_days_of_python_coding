6d2c45f0ab09b7e24ed29a9ea1161d5cc7ad0d45
yaredow <yaredyilma11@gmail.com>
2021-11-27 10:52:24 +0300

    added a function called coins

--- Day 15/Coffe_machine.py
+++ Day 15/Coffe_machine.py
@@ -29,3 +29,34 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+def coins():
+    coin = 0
+    quarter = float(input("how many quarters? "))
+    coin += quarter * 0.25
+    dime = float(input("how many dimes?: "))
+    coin += dime * 0.1
+    nickel = float(input("how many nickels? "))
+    coin += nickel * 0.05
+    penny = float(input("how many pennies? "))
+    coin += penny * 0.01
+    return coin
+
+#TODO: 1. prompt the user by asking what they would like.
+user_choice = input("What would you like? (espresso/latte/cappuccino):\n")
+coins = input("Please insert coins:\n")
+#TODO: 5. Process coins.
+print("Please insert coins.")
+coins()
+
+#TODO: 2. Turn off the Coffee Machine by entering “​off​” to the prompt.
+
+#TODO: 3. Print report.
+
+#TODO: 4. Check resources sufficient?
+
+
+
+#TODO: 6. Check transaction successful?
+
+#TODO: 7. Make Coffee.
