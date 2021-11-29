3c6e68e76b12d457be5eb89aa2b6bedec0f7f708
yaredow <yaredyilma11@gmail.com>
2021-11-29 14:21:36 +0300

    fixed no available module error

--- Day 16/yada.py
+++ Day 16/yada.py
@@ -1,10 +1,14 @@
-from turtle import * 
+# from turtle import * 
 
-my_turtle =Turtle()
-my_turtle.shape("turtle")
-my_turtle.color("red", "yellow")
-my_turtle.forward(100)
-my_screen = Screen()
-print(my_turtle)
-print(my_screen)
-my_screen.exitonclick()
+# my_turtle =Turtle()
+# my_turtle.shape("turtle")
+# my_turtle.color("red", "yellow")
+# my_turtle.forward(100)
+# my_screen = Screen()
+# print(my_turtle)
+# print(my_screen)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+
+table = PrettyTable()
+print(table)
