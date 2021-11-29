f977d496b58a7e65ae17a9f1dd20a6e35fd98f74
yaredow <yaredyilma11@gmail.com>
2021-11-29 20:27:17 +0300

    prettytable alignment

--- Day 16/Turtle_module.py
+++ Day 16/Turtle_module.py
@@ -8,7 +8,13 @@
 # print(my_turtle)
 # print(my_screen)
 # my_screen.exitonclick()
+
 from prettytable import PrettyTable
 
 table = PrettyTable()
+table.field_names = ["Pokeman name", "Type"]
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Squirtle", "Water"])
+table.add_row(["Charmander", "Fire"])
+table.align = "l"
 print(table)
