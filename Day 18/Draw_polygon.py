1a3e3f1b5edd54a6ca2fd867f2e7fd47d513ae3a
yaredow <yaredyilma11@gmail.com>
2021-12-03 13:03:24 +0300

    added a list of colors

--- Day 18/Draw_polygon.py
+++ Day 18/Draw_polygon.py
@@ -1,15 +1,26 @@
 from turtle import *
+import random
+
 tim = Turtle() 
 tim.shape("turtle")
-tim.color("red", "green")
+
+color_list = ["dim gray", "navy", "dark green", "brown", "blue violet", "orange", "dark slate gray", "black", "yellow", "red"]
+
+def color_choice():
+    random.choice(color_list)
+    tim.color(random.choice(color_list))
 
 def draw_polygon(num_side):
     angle = 360 / num_side
     for _ in range(num_side):
         tim.forward(100)
         tim.right(angle)
+        
+
 for num_side in range(3, 11):
+    tim.pensize(5)
     draw_polygon(num_side)
+    color_choice()
 
 screen = Screen()
-screen.exitwhenclick()
+screen.exitonclick()
