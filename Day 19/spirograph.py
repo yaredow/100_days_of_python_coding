110e45ba7e03c0fc068926eaf946f7892ee93875
yaredow <yaredyilma11@gmail.com>
2021-12-05 17:19:41 +0300

    finalized the sketch

--- Day 19/spirograph.py
+++ Day 19/spirograph.py
@@ -3,25 +3,31 @@ from turtle import Turtle, Screen
 timy = Turtle()
 screen = Screen()
 
+def left():
+    timy.backward(10)
+
+def right():
+    current_heading = timy.heading()
+    timy.setheading(current_heading + 10)
+    timy.backward(10)
+
 def move_forward():
     timy.forward(10)
 
 def move_backward():
     timy.backward(10)
 
-def go_up():
-    timy.left(10)
-    timy.forward(10)
-
-def go_down():
-    timy.left(-10)
-    timy.forward(10)
+def clear():
+    timy.clear()
+    timy.penup()
+    timy.home()
+    timy.pendown()
 
 
-
-screen.onkey( move_forward, "d")
-screen.onkey(move_backward, "a")
-screen.onkey(go_up, "w")
-screen.onkey(go_down, "s")
+screen.onkey(right, "Right")
+screen.onkey(left, "Left")
+screen.onkey(move_forward, "Up")
+screen.onkey(move_backward, "Down")
+screen.onkey(clear, "c")
 screen.listen()    
 screen.exitonclick()
