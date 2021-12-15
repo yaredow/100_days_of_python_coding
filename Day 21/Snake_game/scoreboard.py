9a161c728d0c68db05ca56c7c7f252a3c0cf0f78
yaredow <yaredyilma11@gmail.com>
2021-12-15 14:58:15 +0300

    made the scoreboard to score high score

--- Day 21/Snake_game/scoreboard.py
+++ Day 21/Snake_game/scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 ALIGNMENT = "center"
 FONT = ("Courier", 18, "normal")
 
@@ -8,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,21 +18,23 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
 
-    def reset(self):
-        if self.high_score > self.score:
-            self.high_score = self.score
-        self.score = 0
-
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("Game Over", align=ALIGNMENT, font=FONT)
-
     def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
+
+    def reset(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write("{}".format(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
+
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
 
 
