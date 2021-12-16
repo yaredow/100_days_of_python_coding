b31700e61c7d61d916af0b3eefcbd5cebe005e72
yaredow <yaredyilma11@gmail.com>
2021-12-16 10:14:40 +0300

    finalized and created an automated letters

--- Day 24/main.py
+++ Day 24/main.py
@@ -3,10 +3,17 @@ NAMESPACE = "[name]"
 with open("./Input/Names/invited_names.txt") as name_list:
     names = name_list.readlines()
 
+
 with open("./Input/Letters/starting_letter.docx") as starting_letter:
     letter = starting_letter.read()
 
+
 for name in names:
-    replaced_letter = letter.replace(NAMESPACE, name)
+    striped_name = name.strip()
+    replaced_letter = letter.replace(NAMESPACE, striped_name)
+    with open(f"./Output/ReadyToSend/letter_for_{striped_name}.docx", mode="w") as completed_letter:
+        completed_letter.write(letter)
+
+
 
 
