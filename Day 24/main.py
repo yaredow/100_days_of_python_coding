bf5a8a5216ad0d1697d0af8d78c06985caec605c
yaredow <yaredyilma11@gmail.com>
2021-12-16 09:52:07 +0300

    added a placeholder and replace the name with it

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,8 +1,12 @@
-with open("./Input/Letters/starting_letter.docx") as file:
-    starting_letter = file.readlines()
-    print(starting_letter)
+NAMESPACE = "[name]"
 
-with open("./Input/Names/invited_names.txt") as name:
-    names = name.read()
+with open("./Input/Names/invited_names.txt") as name_list:
+    names = name_list.readlines()
+
+with open("./Input/Letters/starting_letter.docx") as starting_letter:
+    letter = starting_letter.read()
+
+for name in names:
+    replaced_letter = letter.replace(NAMESPACE, name)
 
 
