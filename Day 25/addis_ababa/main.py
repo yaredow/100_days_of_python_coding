9333de38d9ba3211c44d6ad5bc990adc6660cad6
yaredow <yaredyilma11@gmail.com>
2021-12-17 14:04:38 +0300

    added a program that let users guess Sheger sub-citie

---
+++ Day 25/addis_ababa/main.py
@@ -0,0 +1,46 @@
+import pandas
+import pandas as pd
+import turtle
+
+screen = turtle.Screen()
+image = "addis_ababa.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+#
+# turtle.mainloop()
+
+num_guess = []
+while len(num_guess) < 10:
+    user_guess = str(screen.textinput(title="{}/10 correctly guessed".format(len(num_guess)), prompt="How many other sub-city can you guess?")).title()
+    cities = pd.read_csv("Sheger_cities.csv")
+    list_of_cities = cities.City.to_list()
+
+    if user_guess in list_of_cities:
+        num_guess.append(user_guess)
+        sheger = turtle.Turtle()
+        sheger.hideturtle()
+        sheger.penup()
+        city_row = cities[cities.City == user_guess]
+        sheger.goto(int(city_row.x), int(city_row.y))
+        sheger.write(user_guess, font=("Courier", 10, "normal"))
+
+    if user_guess == "Exit":
+        correct_guess = []
+        for sub_city in list_of_cities:
+            if sub_city not in num_guess:
+                correct_guess.append(sub_city)
+        break
+
+df = pd.DataFrame(correct_guess)
+df.to_csv("Sub_cities_for_study.csv")
+
+
+
+
+screen.exitonclick()
