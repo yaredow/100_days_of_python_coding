1d4a0db13ef95b29682f5ad76e295028f48b43a7
yaredow <yaredyilma11@gmail.com>
2021-12-22 12:27:04 +0300

    finished adding entry and buttons

--- Day 29/Password_generater/main.py
+++ Day 29/Password_generater/main.py
@@ -5,8 +5,38 @@ from tkinter import *
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title("Konjo password generator")
-canvas = Canvas()
+window.title("Konjo Password Manager")
+window.config(padx=20, pady=20)
+canvas = Canvas(width=200, height=200)
+lock_key_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_key_image)
+canvas.grid(column=1, row=0)
+# creating website label
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+# email and username label
+email_username_label = Label(text="Email/Username: ")
+email_username_label.grid(column=0, row=2)
+# password label
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+# website entry
+website_entry = Entry(width=40)
+website_entry.grid(column=1, row=1, columnspan=2)
+# email and username entry
+email_username_entry = Entry(width=40)
+email_username_entry.grid(column=1, row=2, columnspan=2)
+# password entry
+password_entry = Entry(width=24)
+password_entry.grid(column=1, row=3)
+# password generator button
+generate_password_button = Button(text="Generate Password", highlightthickness=0,  width=12)
+generate_password_button.grid(column=2, row=3)
+# add button
+add_button = Button(text="Add", width=37)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
 
 
 
