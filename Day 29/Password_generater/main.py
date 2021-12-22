f7d9b015ff499f250a85561d0c7cb58714aca566
yaredow <yaredyilma11@gmail.com>
2021-12-22 12:47:11 +0300

    changed the foreground of the canvas

--- Day 29/Password_generater/main.py
+++ Day 29/Password_generater/main.py
@@ -1,12 +1,17 @@
 from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    pass
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Konjo Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 canvas = Canvas(width=200, height=200)
 lock_key_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_key_image)
@@ -22,24 +27,20 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 # website entry
 website_entry = Entry(width=40)
+website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2)
 # email and username entry
 email_username_entry = Entry(width=40)
+email_username_entry.insert(END, string="yaredyilma11@gmail.com")
 email_username_entry.grid(column=1, row=2, columnspan=2)
 # password entry
 password_entry = Entry(width=24)
 password_entry.grid(column=1, row=3)
 # password generator button
-generate_password_button = Button(text="Generate Password", highlightthickness=0,  width=12)
+generate_password_button = Button(text="Generate Password", highlightthickness=0, width=12)
 generate_password_button.grid(column=2, row=3)
 # add button
 add_button = Button(text="Add", width=37)
 add_button.grid(column=1, row=4, columnspan=2)
 
-
-
-
-
-
 window.mainloop()
-
