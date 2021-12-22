27339c368ebadde065cb44fc79230fd40c98cc9f
yaredow <yaredyilma11@gmail.com>
2021-12-22 11:11:24 +0300

    added readme

---
+++ Day 28/README.md
@@ -0,0 +1,4 @@
+## Day 28 of 100 days of code
+
+
+### Intermediate - Building a Password Manager GUI App with Tkinter
---
+++ Day 29/Password_generater/main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Konjo password generator")
+canvas = Canvas()
+
+
+
+
+window.mainloop()
+
