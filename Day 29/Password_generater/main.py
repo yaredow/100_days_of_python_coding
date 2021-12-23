d2a075cbf9cb7061931d8378e11df305ed91de67
yaredow <yaredyilma11@gmail.com>
2021-12-23 20:45:41 +0300

    finalized the password generator

--- Day 29/Password_generater/main.py
+++ Day 29/Password_generater/main.py
@@ -1,11 +1,36 @@
 from tkinter import *
+from tkinter import messagebox
+import pyperclip
+import string
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+    random.shuffle(characters)
+    password = ""
+    for i in range(16):
+        password += random.choice(characters)
+    password_entry.insert(END, string=password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    pass
+    website = website_entry.get()
+    email_username = email_username_entry.get()
+    password = password_entry.get()
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showerror(title="Error", message="Please don't leave any of the field empty")
+    else:
+        is_ok = messagebox.askokcancel(title="Website",
+                                       message=f"These are the information entered:\nEmail: {email_username}\nPassword: {password}")
+        if is_ok:
+            with open("data.txt", "a+") as user_data:
+                user_data.write(f"Website: {website} | Email/Username: {email_username} | Password: {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -37,10 +62,10 @@ email_username_entry.grid(column=1, row=2, columnspan=2)
 password_entry = Entry(width=24)
 password_entry.grid(column=1, row=3)
 # password generator button
-generate_password_button = Button(text="Generate Password", highlightthickness=0, width=12)
+generate_password_button = Button(text="Generate Password", highlightthickness=0, width=12, command=password_generator)
 generate_password_button.grid(column=2, row=3)
 # add button
-add_button = Button(text="Add", width=37)
+add_button = Button(text="Add", width=37, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
---
+++ Day 30/README.md
@@ -0,0 +1,3 @@
+## Day 30 of 100 days of code
+
+### Intermediate - Errors, Exceptions and JSON Data Improving the Password
