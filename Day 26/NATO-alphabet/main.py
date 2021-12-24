e65b56d9e685173b6ca031b721c0d9f34d683c14
yaredow <yaredyilma11@gmail.com>
2021-12-24 10:41:29 +0300

    deleted the nato phonetic folder

--- Day 26/NATO-alphabet/main.py
+++ Day 26/NATO-alphabet/main.py
@@ -1,7 +1,11 @@
 import pandas as pd
+
 data = pd.read_csv("nato_phonetic_alphabet.csv")
-new_dict = {row.letter:row.code for (index, row) in data.iterrows()}
-user_input = input("Please insert your name: ").upper()
-output_list = [new_dict[letter] for letter in user_input]
-print("The substituted phonetic words for your name letters are:")
-print(output_list)
+new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+
+def phonetic_generator():
+    user_input = input("Please insert your name: ").upper()
+    output_list = [new_dict[letter] for letter in user_input]
+    print("The substituted phonetic words for your name letters are:")
+    print(output_list)
