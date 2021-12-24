fc78576f2f5eb55d7aaba2b8220ae8a00c4aa9b5
yaredow <yaredyilma11@gmail.com>
2021-12-24 14:50:01 +0300

    finalized the password manager by adding search option

--- Day 29/Password_generater/main.py
+++ Day 29/Password_generater/main.py
@@ -48,6 +48,24 @@ def save_password():
             password_entry.delete(0, END)
 
 
+# ---------------------------- Find password------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message=f"No data file found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title="email",
+                                message=f"website: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No detail for {website} available")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Konjo Password Manager")
@@ -56,31 +74,28 @@ canvas = Canvas(width=200, height=200)
 lock_key_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_key_image)
 canvas.grid(column=1, row=0)
-# creating website label
+# label
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-# email and username label
 email_username_label = Label(text="Email/Username: ")
 email_username_label.grid(column=0, row=2)
-# password label
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
-# website entry
-website_entry = Entry(width=40)
+# entries
+website_entry = Entry(width=24)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2)
-# email and username entry
+website_entry.grid(column=1, row=1)
 email_username_entry = Entry(width=40)
 email_username_entry.insert(END, string="yaredyilma11@gmail.com")
 email_username_entry.grid(column=1, row=2, columnspan=2)
-# password entry
 password_entry = Entry(width=24)
 password_entry.grid(column=1, row=3)
-# password generator button
+# buttons
 generate_password_button = Button(text="Generate Password", highlightthickness=0, width=12, command=password_generator)
 generate_password_button.grid(column=2, row=3)
-# add button
 add_button = Button(text="Add", width=37, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
+search_button = Button(text="Search", width=12, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
