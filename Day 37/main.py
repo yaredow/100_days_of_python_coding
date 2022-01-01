58a1c36a624ab7c9a518162557e1f5d420bb14cf
yaredow <yaredyilma11@gmail.com>
2022-01-01 12:54:06 +0300

    finished converting the date

---
+++ Day 37/main.py
@@ -0,0 +1,46 @@
+import requests
+import datetime as dt
+
+
+USER_NAME = "yared"
+TOKEN = "hdjeuekskslie"
+GRAPHID = "graph1"
+pixela_endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
+post_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPHID}"
+user_param = {
+    "token": TOKEN,
+    "username": USER_NAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# response = requests.post(url=pixela_endpoint, json=user_param)
+
+graph_param = {
+    "id": GRAPHID,
+    "name": "Coding Graph",
+    "unit": "hour",
+    "type": "float",
+    "color": "ajisai",
+}
+
+# response1 = requests.post(url=graph_endpoint, json=graph_param, headers=headers)
+
+today = dt.datetime.today()
+year = today.strftime("%Y")
+day = today.strftime("%d")
+month = today.strftime("%m")
+date = f"{year}{month}{day}"
+post_pixel_param = {
+    "date": date,
+    "quantity": "2",
+}
+post_header = {
+    "X-USER-TOKEN": TOKEN
+}
+response2 = requests.post(url=post_endpoint, json=post_pixel_param, headers=post_header)
+response2.raise_for_status()
+print(response2.text)
