8e7c8e0f705191cd25afd619ff86993858c2575f
yaredow <yaredyilma11@gmail.com>
2022-02-01 15:51:59 +0300

    Day 54 complated

--- Day 51/main.py
+++ Day 51/main.py
@@ -25,7 +25,7 @@ class InternetSpeedTwitterBot:
         google_sign_in = self.driver.find_element(By.XPATH, '/html/body/div/div/div/div['
                                                             '1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div['
                                                             '2]/div[2]/div[1]/div/div[5]/label/div/div[2]/div/input')
-        google_sign_in.send_keys("yaredyilma11@gmail")
+        google_sign_in.send_keys("yaredyilma2020@outlook.com")
         next_button = self.driver.find_element(By.XPATH,
                                                '/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div['
                                                '2]/div/div/div[2]/div[2]/div[1]/div/div[6]')
@@ -36,19 +36,29 @@ class InternetSpeedTwitterBot:
                                                       '2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div['
                                                       '2]/label/div/div[2]/div/input')
         username.send_keys("@yaredow")
-        password = self.driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div/div/div/div/div/div/div['
-                                                      '2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div['
-                                                      '2]/label/div/div[2]/div/input')
+        another_next_button = self.driver.find_element(By.XPATH,
+                                                       '/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div['
+                                                       '2]/div/div/div[2]/div[2]/div[2]/div/div')
+        another_next_button.click()
+        sleep(3)
+        password = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div['
+                                                      '2]/div/div/div[2]/div[2]/div[1]/div/div[3]/div/label/div/div['
+                                                      '2]/div[1]/input')
         password.send_keys("dopellex11")
         log_in = self.driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div/div/div/div/div/div/div['
                                                     '2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div')
-        sleep(3)
+        log_in.click()
+        sleep(8)
         post = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div['
                                                   '2]/div/div[2]/div[1]/div/div/div/div[2]/div['
                                                   '1]/div/div/div/div/div/div/div/div/div/label/div['
                                                   '1]/div/div/div/div/div[2]/div/div/div/div')
         post_text = f"@ethiotelecom, Why is my internet speed {self.up}mb/s when I pay for {INTERNET_SPEED}mb/s?"
         post.send_keys(post_text)
+        post = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div['
+                                                  '2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div['
+                                                  '3]')
+        post.click()
 
 
 bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
